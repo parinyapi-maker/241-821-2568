57049a4be9e88c32df290185c6b5671410af4e35
parinyapi <parinya.pi@ku.th>
2025-12-30 11:07:37 +0700

    add.js

---
+++ JS/main.js
@@ -0,0 +1,102 @@
+//console.log("Hello World!");
+//console.log("This is a simple JavaScript file.");
+
+//let fname = "JOhn";
+//console.log("Name:",fname);
+//let age = 30;
+//let height = 5.9;
+
+//fname = "Alice";
+//console.log("Name:",fname);
+//fname = "Bob";
+//PI = 3.14159
+//console.log("Name:", fname);
+//console.log("Age:", age);
+//console.log("Height:", height);
+
+//let number1 = '10';
+//let number2 = '3';
+
+//let result1 = number1 + number2;
+//console.log("ผลบวก =", result1);
+
+//** 
+// ==
+// !=
+// >
+// <
+// >=
+// <=
+// */ */
+
+//let number1 = 5;
+//let number2 = 3;
+//let condition = number1 < number2;
+//console.log("condition:", condition);
+
+// if - else condition
+//if (number1 > number2) {
+  //  console.log('this is if');
+//} else if (number1 < number2) {
+  //  console.log('this is if');
+//}
+
+//else {
+//    console.log('this is else');
+//}
+
+/**
+ * Gread
+ * >=80 = A
+ * >=70 = B
+ * >=60 = C
+ * >=50 = D
+ * <50 = F
+ */
+
+/** 
+let scor = prompt('   ');
+ if (scor >=80) {
+    console.log('grade A');
+ } else if (scor >=70) {
+    console.log('grade B');
+ } else if (scor >=60) {
+    console.log('grade C');
+ } else if (scor >=50) {
+    console.log('grade D');
+ } else {
+    console.log('grade F');
+ } 
+*/
+
+/**
+ * && และ
+ * || หรือ
+ * ! not หรือ ไม่
+ */
+
+/** 
+let number1 = 5
+let number2 = 10
+
+let condition1 = (number1 > 0) && (number2 > 0)
+console.log("condition1:", condition1)
+*/
+
+/**
+ * while
+ * for
+ */
+
+/** 
+let counter = 0;
+while (counter <= 5) {
+    counter = counter + 1;
+    //counter +=1;
+    console.log("counter", counter);
+}
+
+for (let i = 0; i <= 4; i = i +1) {
+    console.log("for:", i);
+}
+    
